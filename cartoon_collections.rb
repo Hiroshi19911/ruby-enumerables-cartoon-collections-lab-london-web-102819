24856cd320cac34add18608a40e61de2776db188
Hiroshi Sipahioglu <hiroshis91@icloud.com>
2019-10-22 23:31:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,3 +4,6 @@ def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index {|name, i| puts (i + 1).to_s + ".*#{name}"}
 end	
 
+def summon_captain_planet(produce)# code an argument here
+  # Your code here	  produce.collect {|name| name.capitalize + "!"}
+end	end
